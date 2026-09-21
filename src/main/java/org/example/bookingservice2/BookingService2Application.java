7f3858a347188edc3ea51829afcc8dd3fba0114c
timvetinte <tim.grundstrom@gmail.com>
2026-09-21 12:30:58 +0200

    Changing local folder

---
+++ src/main/java/org/example/bookingservice2/BookingService2Application.java
@@ -0,0 +1,13 @@
+package org.example.bookingservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingService2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bookingservice2/BookingService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookingservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingService2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
